710e68cf244a8689d47e51dd8ca96d84d26f9fa2
Gene Tailor <genetailorvt@gmail.com>
2025-03-16 17:16:20 +0000

    Implemented task based multi threaded texture extraction (5-10% faster with VRoid samples).

--- Assets/UniGLTF/Editor/UniGLTF/ScriptedImporter/TextureExtractor.cs
+++ Assets/UniGLTF/Editor/UniGLTF/ScriptedImporter/TextureExtractor.cs
@@ -1,13 +1,48 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
-using UnityEngine;
-using UnityEditor;
 using System.Linq;
+using System.Threading.Tasks;
+using Unity.Collections;
 using Unity.Profiling;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.Experimental.Rendering;
 
 namespace UniGLTF
 {
+    internal class TextureExtractTaskData
+    {
+        public readonly SubAssetKey key;
+        public readonly UnityPath targetPath;
+        public readonly uint width;
+        public readonly uint height;
+        public readonly NativeArray<byte> imgBytes;
+        public readonly GraphicsFormat graphicsFormat;
+        public Task task;
+
+        public TextureExtractTaskData(SubAssetKey key, UnityPath targetPath, NativeArray<byte> imgBytes, GraphicsFormat graphicsFormat, uint width, uint height)
+        {
+            this.key = key;
+            this.targetPath = targetPath;
+            this.width = width;
+            this.height = height;
+            this.imgBytes = imgBytes;
+            this.graphicsFormat = graphicsFormat;
+        }
+
+        public void Proc()
+        {
+            NativeArray<byte> nativeBytes = ImageConversion.EncodeNativeArrayToPNG(imgBytes, graphicsFormat, width, height);
+
+            // Need to copy NativeArray data to C# array for file writing
+            byte[] bytes = new byte[nativeBytes.Length];
+            nativeBytes.CopyTo(bytes);
+
+            File.WriteAllBytes(targetPath.FullPath, bytes);
+        }
+    }
+
     public class TextureExtractor
     {
         const string TextureDirName = "Textures";
@@ -66,6 +101,33 @@ namespace UniGLTF
             }
         }
 
+        internal bool TryAddTask(SubAssetKey key, TextureDescriptor texDesc, List<TextureExtractTaskData> tasks)
+        {
+            if (Textures.ContainsKey(key))
+            {
+                return false;
+            }
+
+            // Try to add task to convert Texture
+            if (m_subAssets.TryGetValue(key, out var texture) && texture is Texture2D tex2D)
+            {
+                var targetPath = m_textureDirectory.Child($"{key.Name}.png");
+
+                TextureExtractTaskData taskData = new TextureExtractTaskData(key, targetPath, tex2D.GetPixelData<byte>(0), tex2D.graphicsFormat, (uint)tex2D.width, (uint)tex2D.height);
+                taskData.task = Task.Run(taskData.Proc);
+                tasks.Add(taskData);
+
+                Textures.Add(key, targetPath);
+
+                return true;
+            }
+            else
+            {
+                UniGLTFLogger.Warning($"{key} is not converted.");
+                return false;
+            }
+        }
+
         /// <summary>
         ///
         /// * Texture(.png etc...)をディスクに書き出す
@@ -84,22 +146,15 @@ namespace UniGLTF
             s_MarkerStartExtractTextures.Begin();
 
             var extractor = new TextureExtractor(data, textureDirectory, subAssets);
-            try
-            {
-                AssetDatabase.StartAssetEditing();
 
-                foreach (var param in textureDescriptorGenerator.Get().GetEnumerable())
-                {
-                    extractor.Extract(param.SubAssetKey, param);
-                }
-            }
-            catch (Exception e)
+            // Due to overheads of setting up threaded version low core count machines they are likely faster on the non-threaded version
+            if (SystemInfo.processorCount < 4)
             {
-                Debug.LogException(e);
+                Extract(textureDescriptorGenerator, extractor);
             }
-            finally
+            else
             {
-                AssetDatabase.StopAssetEditing();
+                ExtractThreaded(textureDescriptorGenerator, extractor);
             }
 
             s_MarkerStartExtractTextures.End();
@@ -127,6 +182,66 @@ namespace UniGLTF
                     onCompleted(extractor.Textures.Values);
                 }
             };
+        }
+
+        private static void Extract(ITextureDescriptorGenerator textureDescriptorGenerator, TextureExtractor extractor)
+        {
+            try
+            {
+                AssetDatabase.StartAssetEditing();
+                foreach (var param in textureDescriptorGenerator.Get().GetEnumerable())
+                {
+                    extractor.Extract(param.SubAssetKey, param);
+                }
+            }
+            catch (Exception e)
+            {
+                Debug.LogException(e);
+            }
+            finally
+            {
+                AssetDatabase.StopAssetEditing();
+            }
+        }
+
+        private static void ExtractThreaded(ITextureDescriptorGenerator textureDescriptorGenerator, TextureExtractor extractor)
+        {
+            List<TextureExtractTaskData> taskDatas = new List<TextureExtractTaskData>();
+            foreach (var param in textureDescriptorGenerator.Get().GetEnumerable())
+            {
+                extractor.TryAddTask(param.SubAssetKey, param, taskDatas);
+            }
+
+            int count = taskDatas.Count;
+            if (count > 0)
+            {
+                Task[] tasks = new Task[count];
+                for (int i = 0; i < count; i++)
+                {
+                    tasks[i] = taskDatas[i].task;
+                }
+
+                // Wait for all the tasks to finish
+                Task.WaitAll(tasks);
+
+                try
+                {
+                    AssetDatabase.StartAssetEditing();
+                    // Import asset
+                    foreach (var taskData in taskDatas)
+                    {
+                        taskData.targetPath.ImportAsset();
+                    }
+                }
+                catch (Exception e)
+                {
+                    Debug.LogException(e);
+                }
+                finally
+                {
+                    AssetDatabase.StopAssetEditing();
+                }
+            }
         }
     }
 }
